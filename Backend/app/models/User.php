73a8f7bec4ed6454d66bb92a9c837102d02c59cd
Sun Rosa <rozasun61@gmail.com>
2025-03-10 00:28:56 +0700

    UserModel

--- Backend/app/models/User.php
+++ Backend/app/models/User.php
@@ -2,27 +2,50 @@
 
 require_once __DIR__ . "../../config/database.php";
 
-
 class User{
     private $conn;
-    public function __construct(){
-        global $conn;
-        $this->conn = $conn;
+    public function __construct(Database $database){
+        $this->conn = $database->getConnection();
     }
 
     //  get All users
-   public function getAllUsers(){
-       $sql = "SELECT * FROM uusers";
-       $result = $this->conn->quary($sql);
-       return $result->fetch_all(MYSQLI_ASSOC);
-   }
-
-//    get users by ID 
-    public function getUserByID($id){
-         $sql = "SELECT * FROM uusers WHERE id = ? ";
-        $stmt = $this->conn->prepare($sql);
-        $stmt->bind_param("i",$id);
-        $stmt->execute();
-        return $stmt->get_result()->fetch_assoc( );
+    public function getAllUsers()
+    {
+        $sql = "SELECT * FROM uusers";
+        $result = $this->conn->query($sql);
+        return $result->fetchall(PDO::FETCH_ASSOC);
     }
+
+    //    get users by ID 
+    public function getUserByID($id)
+    {
+        $sql = "SELECT * FROM uusers WHERE id = ? ";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bindparam("i", $id);
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+    //  register method 
+    public function register($name, $email, $password, $role)
+    {
+        $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
+        $sql  = "INSERT INTO uuser (name , email , password , role) VALUES ( ? , ? , ? , ?)";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bindparam("ssss", $name, $email, $hashedPassword, $role);
+        return $stmt->execute();
+    }
+    //  Login method
+    public function login($email, $password){
+        $sql = "SELECT * FROM user WHERE email = ? ";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bindparam("s", $email);
+        $stmt->execute();
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+        if ($result && password_verify($password, $result['password'])) {
+            return $result;
+        }
+        return null;
+    }
+
+    
 }
