0b7261ddf076788f41c93be6e09210340e981520
Sun Rosa <rozasun61@gmail.com>
2025-03-23 22:11:36 +0700

    Main Content

--- Frontend/src/components/MainHero.tsx
+++ Frontend/src/components/MainHero.tsx
@@ -1,11 +1,25 @@
 import Navbar from "./Home/Navbar";
+import { useEffect, useState } from "react";
 import SlideCarousel from "./Home/SlideCarousel";
 
+interface Property {
+  id: number;
+  photo: string;
+  title: string;
+  description: string;
+}
 const MainHero = () => {
+  const [slide, setSlide] = useState<Property[]>([]);
+  useEffect(() => {
+    fetch("/Slide.json")
+      .then((responese) => responese.json())
+      .then((data) => setSlide(data))
+      .catch((error) => console.error("Error:", error));
+  }, []);
   return (
-    <section className="wrapper w-full">
+    <section className="w-full ">
       <Navbar />
-      <SlideCarousel />
+      <SlideCarousel Data={slide} />
     </section>
   );
 };
