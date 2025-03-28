a80f43f797445399fc502369639bdf551018651f
NarakCODE <channarakluy@gmail.com>
2025-03-28 18:20:28 +0700

    component

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,10 +1,16 @@
+import Layout from "./layout/Layout";
 import Home from "./pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
