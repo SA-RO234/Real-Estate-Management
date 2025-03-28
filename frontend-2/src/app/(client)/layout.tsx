d0a9d59514576a69e0784070260447c27de434ab
NarakCODE <channarakluy@gmail.com>
2025-03-28 20:06:42 +0700

    feat(comp) : Navbar

---
+++ frontend-2/src/app/(client)/layout.tsx
@@ -0,0 +1,15 @@
+import Footer from "@/components/shared/Footer";
+import { Navbar } from "@/components/shared/Navbar";
+import React from "react";
+
+const ClientLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <Navbar />
+      <main className="container px-4 mx-auto">{children}</main>
+      <Footer />
+    </>
+  );
+};
+
+export default ClientLayout;
