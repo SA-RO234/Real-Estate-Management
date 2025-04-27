b4f349072118e1feb49ed7deb45b0166d33aab1e
Sun Rosa <rozasun61@gmail.com>
2025-04-27 23:23:47 +0700

    New Update

--- frontend-2/src/app/(admin)/property/page.tsx
+++ frontend-2/src/app/(admin)/property/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {DataTablePerperty } from "@/components/common/admin/data-table";
+import { DataTablePerperty } from "@/components/common/admin/data-table";
 import data from "../dashboard/data.json";
 const PropertyPage = () => {
   return (
@@ -7,7 +7,7 @@ const PropertyPage = () => {
       <h2 className="pb-2 text-3xl font-semibold tracking-tight transition-colors">
         Property Management
       </h2>
-       <DataTablePerperty data={data} />
+      <DataTablePerperty data={data} />
     </div>
   );
 };
