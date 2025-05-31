ab708cc3d4d53c1a5e7575bf7928165dd94872f4
Sun Rosa <rozasun61@gmail.com>
2025-05-31 10:29:49 +0700

    Admin

--- frontend-2/src/app/admin/next.config.ts
+++ frontend-2/src/app/admin/next.config.ts
@@ -1,18 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "**",
-        port: "",
       },
     ],
   },
   experimental: {
-    scrollRestoration: false, // Ensure this is false to reset scroll position
+    scrollRestoration: false,
   },
 };
 
