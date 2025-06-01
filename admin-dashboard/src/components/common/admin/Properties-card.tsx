0ea45530cfeaf840463771cc234e1bb543f41d71
Sun Rosa <rozasun61@gmail.com>
2025-06-01 18:21:59 +0700

    Admin

--- admin-dashboard/src/components/common/admin/Properties-card.tsx
+++ admin-dashboard/src/components/common/admin/Properties-card.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 // import Image from "next/image";
-import { Property } from "@/lib/types";
+import { Property } from "@/app/lib/types";
 
 export default function PropertyCard({ property }: { property: Property }) {
   return (
