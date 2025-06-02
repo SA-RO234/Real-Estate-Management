1a6b9e788e30a928642a5ca97044a9586b058216
Sun Rosa <rozasun61@gmail.com>
2025-06-02 11:40:58 +0700

    New Version

--- Backend/config/database.php
+++ Backend/config/database.php
@@ -4,8 +4,9 @@ require_once __DIR__ . "/../vendor/autoload.php";
 use Dotenv\Dotenv;
 
 // Load environment variables
-$dotenv = Dotenv::createImmutable(__DIR__ . "/../");
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
+
 
 class Database
 {
