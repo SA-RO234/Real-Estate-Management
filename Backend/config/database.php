d33f7e6348630d9f11dba8fa01b56b6057fd7852
Sun Rosa <rozasun61@gmail.com>
2025-06-02 12:18:47 +0700

    New Version

--- Backend/config/database.php
+++ Backend/config/database.php
@@ -29,7 +29,7 @@ class Database
     {
         $this->conn = null;
         try {
-            $dsn = "mysql:host=" . $this->host . ";dbname=" . $this->db_name . ";charset=utf8mb4";
+            $dsn = "mysql:host=" . $this->host . ";port=3306;dbname=" . $this->db_name . ";charset=utf8mb4";
             $this->conn = new PDO($dsn, $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $this->conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
@@ -41,4 +41,3 @@ class Database
         return $this->conn;
     }
 }
-
